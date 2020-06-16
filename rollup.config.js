f06466dcd7460a4a90c6faa2f27a2b04b42aedd5
PurpleMyst <PurpleMyst@users.noreply.github.com>
2020-06-16 16:45:38 +0200

    chore: dist/ -> docs/ for gh pages

--- rollup.config.js
+++ rollup.config.js
@@ -6,7 +6,7 @@ import { terser } from "rollup-plugin-terser";
 export default {
   input: "src/index.ts",
   output: {
-    file: "dist/index.js",
+    file: "docs/index.js",
     format: "iife",
   },
   plugins: [
@@ -17,7 +17,7 @@ export default {
       targets: [
         {
           src: ["src/index.html", "src/index.css", "src/melog.png"],
-          dest: "dist",
+          dest: "docs",
         },
       ],
     }),
